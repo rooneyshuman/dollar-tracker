abb6eb2ac048e21955e4563688c635b3d69e8c3b
David Munoz Constantine <dmunoz.dmc@gmail.com>
2020-02-07 17:49:55 -0800

    factored some commented out code

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask
 from flask.views import MethodView
 from index import Index
 from sassutils.wsgi import SassMiddleware
-import sass
+
+
 app = Flask(__name__)
 app.secret_key == os.urandom(24)
 app.add_url_rule("/", view_func=Index.as_view("index"), methods=["GET"])
 app.wsgi_app = SassMiddleware(app.wsgi_app, {__name__: ('static/sass', 'static/stylesheets', 'static/stylesheets')})
 
 if __name__ == "__main__":
-    # sass.compile(dirname=('static/sass', 'static/stylesheets'), source_comments=True)
     app.run(debug=True)
 
