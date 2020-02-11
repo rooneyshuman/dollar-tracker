c9a1f82347a551dd62baa557ea79f280dfa9e3c5
rooneyshuman <rooneyshuman@gmail.com>
2020-02-11 11:07:46 -0800

    Retrieve environment variables from .env file

--- app.py
+++ app.py
@@ -3,13 +3,29 @@ from flask import Flask
 from flask.views import MethodView
 from index import Index
 from sassutils.wsgi import SassMiddleware
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 app.secret_key == os.urandom(24)
+app.wsgi_app = SassMiddleware(
+    app.wsgi_app,
+    {__name__: ("static/sass", "static/stylesheets", "static/stylesheets")},
+)
+
+# Load .env file and save data to corresponding variables
+load_dotenv()
+PLAID_CLIENT_ID = os.getenv("PLAID_CLIENT_ID")
+PLAID_SECRET = os.getenv("PLAID_SECRET")
+PLAID_PUBLIC_KEY = os.getenv("PLAID_PUBLIC_KEY")
+PLAID_ENV = os.getenv("PLAID_ENV")
+PLAID_PRODUCTS = os.getenv("PLAID_PRODUCTS")
+PLAID_COUNTRY_CODES = os.getenv("PLAID_COUNTRY_CODES")
+PLAID_OAUTH_REDIRECT_URI = os.getenv("PLAID_OAUTH_REDIRECT_URI", "")
+PLAID_OAUTH_NONCE = os.getenv("PLAID_OAUTH_NONCE", "")
+
+
 app.add_url_rule("/", view_func=Index.as_view("index"), methods=["GET"])
-app.wsgi_app = SassMiddleware(app.wsgi_app, {__name__: ('static/sass', 'static/stylesheets', 'static/stylesheets')})
 
 if __name__ == "__main__":
     app.run(debug=True)
-
