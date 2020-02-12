0aca97a3d1fa213a6b548672aa155f124830b05b
rooneyshuman <rooneyshuman@gmail.com>
2020-02-11 20:38:47 -0800

    Refactor account info retrieval to use Accounts endpoint instead of Auth. Remove unnecessary account number vars

--- index.py
+++ index.py
@@ -14,35 +14,21 @@ class Index(MethodView):
 
     def get_account_data(self):
         """
-        Returns list of dictionaries containing account the following data: 
-            name, balance, account_id, type, account_no, routing_no
+        Returns list of dictionaries containing account the following data: name, balance, type
         :return: list of retrieved accounts
         """
 
-        # Retrive raw account data through Plaid's "Auth" endpoint
-        raw_acct_data = client.Auth.get(ACCESS_TOKEN)
+        # Retrive raw account data through Plaid's "Accounts" endpoint
+        raw_acct_data = client.Accounts.get(ACCESS_TOKEN)
 
         # Create dictionary with accounts' information
         accounts = [
             dict(
                 name=raw_acct["name"],
                 balance=raw_acct["balances"]["current"],
-                id=raw_acct["account_id"],
-                type=raw_acct["subtype"],
+                type=raw_acct["type"],
             )
             for raw_acct in raw_acct_data["accounts"]
         ]
-
-        # Retrieve account and routing numbers using ACH fields (US standard)
-        if len(raw_acct_data["numbers"]["ach"]) > 0:
-            for acct in accounts:
-                acct_no = list(
-                    filter(
-                        lambda ach_nums: ach_nums["account_id"] == acct["id"],
-                        raw_acct_data["numbers"]["ach"],
-                    )
-                )
-                acct["account_no"] = acct_no[0]["account"] if len(acct_no) > 0 else ""
-                acct["routing_no"] = acct_no[0]["routing"] if len(acct_no) > 0 else ""
 
         return accounts
