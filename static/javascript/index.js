1d21b4c0dfdc6b0a5e77bccc948fe18b6ad893c7
rooneyshuman <rooneyshuman@gmail.com>
2020-03-02 22:23:41 -0800

    Retrieve and categorize assets and liabilities data; draw donut graphs

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,17 +1,67 @@
+/**
+ * Creates assets and liabilities charts
+ */
 google.charts.load("current", {
   packages: ["corechart"]
 });
 google.charts.setOnLoadCallback(drawChart);
 
+var assets = [
+  ['Account', 'Balance']
+];
+var liabilities = [
+  ['Account', 'Balance']
+];
+
+function net_worth_calc(accounts) {
+  get_assets(accounts);
+  get_liabilities(accounts);
+}
+
+// Assets retrieval and calculations
+function get_assets(accounts) {
+  var retirement = accounts.filter(e => e.type == "investment");
+  var ret_total = 0;
+  for (let acct of retirement) {
+    ret_total += acct['balance'];
+  }
+  assets.push(['Retirement', ret_total]);
+
+  var personal = accounts.filter(e => e.subtype == "savings" || e.subtype == "checking");
+  var pers_total = 0;
+  for (let acct of personal) {
+    pers_total += acct['balance'];
+  }
+  assets.push(['Personal', pers_total]);
+
+  var investments = accounts.filter(e => e.subtype == "savings" || e.subtype == "money market" || e.subtype == "cd");
+  var inv_total = 0;
+  for (let acct of investments) {
+    inv_total += acct['balance'];
+  }
+  assets.push(['Investments', inv_total]);
+}
+
+// Liabilities retrieval and calculations
+function get_liabilities(accounts) {
+  var credit = accounts.filter(e => e.type == "credit");
+  var cred_total = 0;
+  for (let acct of credit) {
+    cred_total += acct['balance'];
+  }
+  liabilities.push(['Credit', cred_total]);
+
+  var loans = accounts.filter(e => e.type == "loan");
+  var loan_total = 0;
+  for (let acct of loans) {
+    loan_total += acct['balance'];
+  }
+  liabilities.push(['Loans', loan_total]);
+}
+
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work', 11],
-    ['Eat', 2],
-    ['Commute', 2],
-    ['Watch TV', 2],
-    ['Sleep', 7]
-  ]);
+  var assets_chart_data = google.visualization.arrayToDataTable(assets);
+  var liabilities_chart_data = google.visualization.arrayToDataTable(liabilities);
 
   var options = {
     pieHole: .5,
@@ -19,8 +69,8 @@ function drawChart() {
     legend: 'none'
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  chart.draw(data, options);
-  chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
-  chart.draw(data, options);
+  var chart = new google.visualization.PieChart(document.getElementById('assets_chart'));
+  chart.draw(assets_chart_data, options);
+  chart = new google.visualization.PieChart(document.getElementById('liabilities_chart'));
+  chart.draw(liabilities_chart_data, options);
 }
