cc60fc246e05dbb23d8ba0f24e5988b1a5edc2b3
rooneyshuman <rooneyshuman@gmail.com>
2020-03-03 15:50:25 -0800

    Reformatted javascript files with Beautify

--- static/javascript/portfolio.js
+++ static/javascript/portfolio.js
@@ -1,68 +1,82 @@
 //chart can only be drawn when dom is loaded, maybe dom loads faster than google charts, not sure if thats the case
 //every time
-document.addEventListener("DOMContentLoaded", function(event) {
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(chartStocks);
+document.addEventListener("DOMContentLoaded", function (event) {
+  google.charts.load("current", {
+    packages: ["corechart"]
+  });
+  google.charts.setOnLoadCallback(chartStocks);
 });
 
-function chartStocks(){
-    for (let i = 0; i < symbols.length; i++) {
-        let chart = new google.visualization.CandlestickChart(document.getElementById(`chart-div-${i}`));
-        drawOHLC(chart, symbols[i], ranges[i])
-    }
+function chartStocks() {
+  for (let i = 0; i < symbols.length; i++) {
+    let chart = new google.visualization.CandlestickChart(
+      document.getElementById(`chart-div-${i}`)
+    );
+    drawOHLC(chart, symbols[i], ranges[i]);
+  }
 }
-function drawOHLC(chart, symbol, range){
-    const IEX_KEY = "pk_2d3798cff29b4669a4b6a4b0b41b2cf2"; //this is the public key, ok to share since I have free plan
-    fetch(`https://cloud.iexapis.com/v1/stock/${symbol}/chart/${range}?token=${IEX_KEY}`)
-    .then(function(response){
-        if(response.ok){
-            return response.json();
-        }
-        return Promise.reject(response.ok);
-    })
-    .then(function(data){
-        let prices = [];
-        for(let tick of data){
-            prices.push([   tick['date'],
-                            tick['low'],
-                            tick['open'],
-                            tick['close'],
-                            tick['high']]);
-        }
-        let options = {
-            legend:'none',
-            titleTextStyle: {
-                fontSize: 24
-            },
-            candlestick: {
-                fallingColor: { strokeWidth: 0, fill: '#1c84c6' }, 
-                risingColor: { strokeWidth: 0, fill: '#23c6c8' } 
-            },
-            colors: ['black'],
-            animation:{
-                duration: 500,
-                easing: 'out',
-                startup: true
-            },
-            //needed for chart to take whole viewport
-            chartArea: {
-                width: '84%',
-                height: '90%'
-            },
-            width: '90%',
-            hAxis:{
-                textPosition: 'none'
 
-            },
-            vAxis:{
-                format: '$##',
-                title: 'Price'
-            }
-        };
-        chart.draw(google.visualization.arrayToDataTable(prices, true), options);
+function drawOHLC(chart, symbol, range) {
+  const IEX_KEY = "pk_2d3798cff29b4669a4b6a4b0b41b2cf2"; //this is the public key, ok to share since I have free plan
+  fetch(
+      `https://cloud.iexapis.com/v1/stock/${symbol}/chart/${range}?token=${IEX_KEY}`
+    )
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response.ok);
     })
-    .catch(function(error){
-        //TODO add error message on html
-        console.log("Error retrieving from IEX: \n", error);
+    .then(function (data) {
+      let prices = [];
+      for (let tick of data) {
+        prices.push([
+          tick["date"],
+          tick["low"],
+          tick["open"],
+          tick["close"],
+          tick["high"]
+        ]);
+      }
+      let options = {
+        legend: "none",
+        titleTextStyle: {
+          fontSize: 24
+        },
+        candlestick: {
+          fallingColor: {
+            strokeWidth: 0,
+            fill: "#1c84c6"
+          },
+          risingColor: {
+            strokeWidth: 0,
+            fill: "#23c6c8"
+          }
+        },
+        colors: ["black"],
+        animation: {
+          duration: 500,
+          easing: "out",
+          startup: true
+        },
+        //needed for chart to take whole viewport
+        chartArea: {
+          width: "84%",
+          height: "90%"
+        },
+        width: "90%",
+        hAxis: {
+          textPosition: "none"
+        },
+        vAxis: {
+          format: "$##",
+          title: "Price"
+        }
+      };
+      chart.draw(google.visualization.arrayToDataTable(prices, true), options);
+    })
+    .catch(function (error) {
+      //TODO add error message on html
+      console.log("Error retrieving from IEX: \n", error);
     });
 }
