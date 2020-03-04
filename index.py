f63a188fcf23932ab38882598f800d6a1965b504
rooneyshuman <rooneyshuman@gmail.com>
2020-03-03 21:15:36 -0800

    Create method for retrieving transaction info

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask.views import MethodView
 from settings import *
+import datetime
 
 
 class Index(MethodView):
@@ -33,3 +34,32 @@ class Index(MethodView):
         ]
 
         return accounts
+
+    def get_transaction_data(self):
+        """
+        Returns list of dictionaries containing the following transaction data: name, amount, category, date
+        :return: list of retrieved transactions
+        """
+
+        # Retrive raw account data through Plaid's "Accounts" endpoint
+        # Pull transactions for the last 30 days
+        start_date = "{:%Y-%m-%d}".format(
+            datetime.datetime.now() + datetime.timedelta(-30)
+        )
+        end_date = "{:%Y-%m-%d}".format(datetime.datetime.now())
+        transactions_response = client.Transactions.get(
+            ACCESS_TOKEN, start_date, end_date
+        )
+
+        # Create dictionary with transactions' information
+        transactions = [
+            dict(
+                amount=transaction["amount"],
+                name=transaction["name"],
+                date=transaction["date"],
+                category=transaction["category"][0],
+            )
+            for transaction in transactions_response["transactions"]
+        ]
+
+        return transactions
