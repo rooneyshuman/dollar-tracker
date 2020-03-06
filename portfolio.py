e0c4c622d793ad18b83ca2187458451d90dae195
David Munoz Constantine <dmunoz.dmc@gmail.com>
2020-03-05 21:52:32 -0800

    added news feed and fixed some visual issues

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,13 @@
 from flask import render_template
 from flask.views import MethodView
 import requests
+from iexfinance.stocks import Stock
+from datetime import datetime
 
 
 class Portfolio(MethodView):
+
+
     def get(self):
         """
         GET method for portfolio page. config dict contains stocks to chart
@@ -16,12 +20,14 @@ class Portfolio(MethodView):
         allocations = [30, 10, 25, 25, 5, 5]
         config = {"symbols": symbols, "ranges": ranges, "sectors": sectors, "allocations": allocations}
         messages = self.getTwits()
+        news_messages = self.getNews()
         return render_template(
             "portfolio.html",
             page_name="Portfolio",
             config=config,
             config_len=len(symbols),
             messages=messages,
+            news_messages=news_messages,
         )
 
     def getTwits(self):
@@ -31,3 +37,11 @@ class Portfolio(MethodView):
             return data["messages"]
         else:
             return {"error": r.status_code}
+
+    def getNews(self):
+        IEX_KEY = "pk_2d3798cff29b4669a4b6a4b0b41b2cf2"
+        spy = Stock("SPY", token=IEX_KEY)
+        allNews = spy.get_news()
+        for news in allNews:
+            news['datetime'] = datetime.fromtimestamp(news['datetime']/1000).strftime("%m/%d/%Y")
+        return allNews
